f7d5be4b6d88da12b36984059d5f7a5c6f448e69
John Arévalo <j2atdev@hotmail.com>
2021-04-18 00:15:09 -0500

    End project

--- BlazorNetCore/Data/Entities/Customer.cs
+++ BlazorNetCore/Data/Entities/Customer.cs
@@ -6,16 +6,16 @@ namespace BlazorNetCore.Data.Entities
     {
         public int Id { get; set; }
 
-        [MaxLength(15, ErrorMessage = "El {0} campo no puede tener mas de {1} carácteres.")]
-        [Required(ErrorMessage = "El {0} campo es obligatorio.")]
+        [MaxLength(15, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Customer_id { get; set; }
 
-        [MaxLength(100, ErrorMessage = "El {0} campo no puede tener mas de {1} carácteres.")]
-        [Required(ErrorMessage = "El {0} campo es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Customer_name { get; set; }
 
-        [MaxLength(100, ErrorMessage = "El {0} campo no puede tener mas de {1} carácteres.")]
-        [Required(ErrorMessage = "El {0} campo es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Customer_city { get; set; }
     }
 }
